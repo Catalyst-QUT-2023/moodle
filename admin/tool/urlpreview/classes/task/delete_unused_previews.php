aef5aabe19fb0d86ad6b5a1ec787bf81d678fea6
emnurul <emilylim.cm@gmail.com>
2023-09-12 12:01:42 +1000

    Created url preview tool

---
+++ admin/tool/urlpreview/classes/task/delete_unused_previews.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package     tool_urlpreview
+ * @copyright   2023 Thomas Daly <n11134551@qut.edu.au>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace tool_urlpreview\task;
+
+use core\task\scheduled_task;
+use tool_urlpreview\urlpreview;
+
+
+defined('MOODLE_INTERNAL') || die();
+
+class delete_unused_previews extends scheduled_task {
+
+    public function get_name() {
+        return get_string('deleteunusedpreviews', 'tool_urlpreview');
+    }
+
+    public function execute() {
+        global $DB;
+        $threemonthsago = time() - (3 * 30 * 24 * 60 * 60); // Roughly 3 months.
+        $DB->delete_records_select('urlpreview', 'lastpreviewed < ?', [$threemonthsago]);
+    }
+}
---
+++ lib/classes/url/unfurler.php
@@ -0,0 +1,149 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * Receives Open Graph protocol metadata (a.k.a social media metadata) from link
+ *
+ * @package    core
+ * @copyright  2021 Jon Green <jgreen01@stanford.edu>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+require_once('../../../config.php');
+require_once($CFG->libdir.'/filelib.php');
+
+class unfurl {
+    public $title = '';
+    public $sitename = '';
+    public $image = '';
+    public $description = '';
+    public $canonicalurl = '';
+    public $type = '';
+    public $noogmetadata = true;
+    private $response;
+
+    public function __construct($url) {
+
+        // Initialize cURL session
+        $curl = new curl();
+        $options = array(
+            'CURLOPT_RETURNTRANSFER' => true,
+            'CURLOPT_TIMEOUT' => 5
+        );
+        $this->response = $curl->get($url, null, $options);
+
+        $curlresponse = $this->response;
+
+        $error_no = $curl->get_errno();
+        if ($error_no === CURLE_OPERATION_TIMEOUTED) {
+            echo "Timeout occurred while fetching URL: $url"; 
+            return;
+        }
+        
+        $this->extract_html_metadata($url,$curlresponse);
+
+    
+        
+    }
+
+    public function extract_html_metadata($url, $responseurl){
+        $doc = new DOMDocument();
+        @$doc->loadHTML('<?xml encoding="UTF-8">' . $responseurl);
+        $metataglist = $doc->getElementsByTagName('meta');
+
+        //set default values
+        //default html title
+        $titleElement = $doc->getElementsByTagName('title')->item(0);
+        $h1Element = $doc->getElementsByTagName('h1')->item(0);
+        $h2Element = $doc->getElementsByTagName('h2')->item(0);
+
+        if ($titleElement) {
+            $this->title = $titleElement->textContent;
+        } elseif ($h1Element) {
+            $this->title = $h1Element->textContent;
+        } elseif ($h2Element) {
+            $this->title = $h2Element->textContent;
+        }
+
+        //iterate through meta tags
+        foreach ($metataglist as $metatag) {
+            $propertyattribute = strtolower(s($metatag->getAttribute('property')));
+            if (
+                !empty($propertyattribute) &&
+                preg_match ('/^og:\w/i', $propertyattribute) === 1
+            ) {
+                $this->noogmetadata = false;
+                break;
+            }
+        }
+
+        if ($this->noogmetadata) {
+            return;
+        }
+
+        foreach ($metataglist as $metatag) {
+            $propertyattribute = strtolower(s($metatag->getAttribute('property')));
+            $contentattribute = s($metatag->getAttribute('content'));
+            if (
+                !empty($propertyattribute) &&
+                !empty($contentattribute) &&
+                preg_match ('/^og:\w/i', $propertyattribute) === 1
+            ) {
+                switch ($propertyattribute) {
+                    case 'og:title':
+                        $this->title = $contentattribute;
+                        break;
+                    case 'og:site_name':
+                        $this->sitename = $contentattribute;
+                        break;
+                    case 'og:image':
+                        $imageurlparts = parse_url($contentattribute);
+                        // Some websites only give the path.
+                        if (empty($imageurlparts['host']) && !empty($imageurlparts['path'])) {
+                            $urlparts = parse_url($url);
+                            $this->image = $urlparts['scheme'].'://'.$urlparts['host'].$imageurlparts['path'];
+                        } else {
+                            $this->image = $contentattribute;
+                        }
+                        break;
+                    case 'og:description':
+                        $this->description = $contentattribute;
+                        break;
+                    case 'og:url':
+                        $this->canonicalurl = $contentattribute;
+                        break;
+                    case 'og:type':
+                        $this->type = $contentattribute;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+    
+    public function render_unfurl_metadata() {
+        global $OUTPUT;  // Use the global $OUTPUT variable, Moodle's core renderer.
+
+        // Get the properties of this object as an array.
+        $unfurldata = get_object_vars($this);
+
+        // Use the render_from_template method to render Mustache template.
+        return $OUTPUT->render_from_template('tool_urlpreview/metadata', $unfurldata);
+    }
+
+}
